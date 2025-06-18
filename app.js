015b47fb1ca92c8e64b1d48bcc7419d04884ffaf
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-06-18 19:42:28 +0545

    add Express.js server setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World sage!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Subscription Tracker API!")
+})
+
+app.listen(3000, () => {
+    console.log("Subscription Tracker API is running on port http://localhost:3000")
+})
+
+export default app;
